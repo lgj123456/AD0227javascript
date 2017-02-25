b20964d7318ed68e58c8b12d27ea941110d4c6ba
yhdj <1079652907@qq.com>
2017-02-25 16:51:36 +0800

    第四次提交-申请动态权限

--- app/src/main/java/com/example/yhdj/ad0227js/MainActivity.java
+++ app/src/main/java/com/example/yhdj/ad0227js/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.yhdj.ad0227js;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -19,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_js1;
     private Button btn_js2;
     private WebView mWebView;
+    private final int REQUEST_CODE_CALL_PERMISSION = 0;
+    private final int REQUEST_CODE_SEND_MSG = 1;
+    private boolean isCanCall = false;
+    private boolean isCanSendMsg = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +49,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mWebView.loadUrl("javascript:javacalljs()");
+
             }
         });
+
+
 
         btn_js2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +63,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    //检查是否有打电话的权限
+    private boolean ishasCallPermission() {
+        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+    }
+
+    private boolean ishasSendMsgPermission() {
+        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+    }
+
+    private void applyCallPermission(){
+        String permissions[] = {Manifest.permission.CALL_PHONE,Manifest.permission.SEND_SMS};
+        ActivityCompat.requestPermissions(this,permissions,REQUEST_CODE_CALL_PERMISSION);
+    }
+
     @JavascriptInterface
     public void startFunction(){
         runOnUiThread(new Runnable() {
@@ -74,6 +100,16 @@ public class MainActivity extends AppCompatActivity {
 
     @JavascriptInterface
     public void call(String num){
+        if(!ishasCallPermission()){
+            applyCallPermission();
+        }
+        if(num.isEmpty()){
+            Toast.makeText(this, "电话号码不能为空！！！", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!isCanCall){
+            return;
+        }
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_CALL);
         intent.setData(Uri.parse("tel:" + num));
@@ -82,6 +118,16 @@ public class MainActivity extends AppCompatActivity {
 
     @JavascriptInterface
     public void sendMsg(String num, String msg){
+        if(!ishasSendMsgPermission()){
+            applyCallPermission();
+        }
+        if(msg.isEmpty() || num.isEmpty()){
+            Toast.makeText(this, "电话号码和短信内容不能为空！！！", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!isCanSendMsg){
+            return;
+        }
         SmsManager smsManager = SmsManager.getDefault();
         ArrayList<String> list = smsManager.divideMessage(msg);
         for(String text : list){
@@ -91,4 +137,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+       switch (requestCode){
+           case REQUEST_CODE_CALL_PERMISSION:
+               if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                   Toast.makeText(this, "申请打电话权限成功！！！", Toast.LENGTH_SHORT).show();
+                   isCanCall = true;
+
+               }else{
+                   Toast.makeText(this, "申请打电话权限失败！！！", Toast.LENGTH_SHORT).show();
+               }
+               break;
+           case REQUEST_CODE_SEND_MSG :
+               if (grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                   Toast.makeText(this, "申请发短信权限成功！！！", Toast.LENGTH_SHORT).show();
+                   isCanSendMsg = true;
+               }else{
+                   Toast.makeText(this, "申请发短信权限失败！！！", Toast.LENGTH_SHORT).show();
+
+               }
+               break;
+       }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
